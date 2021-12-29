e52a1734518acec9485f56eb923fbc1765f65a3e
Hyeongdong Kim <gudehdu10@gmail.com>
2021-12-29 16:38:23 +0900

    hd

--- reactapp/src/components/page/Join.js
+++ reactapp/src/components/page/Join.js
@@ -3,63 +3,61 @@ import React, { useRef } from "react";
 
 function Join() {
 
-    const userUrl = "http://localhost:8080/user/save";
-    const userId = useRef();
-    const userAddress = useRef();
-    const userPassword = useRef();
-    const userAdmin = useRef();
-    
+	const userUrl = "http://localhost:8080/user/save";
+	const userId = useRef();
+	const userAddress = useRef();
+	const userPassword = useRef();
 
-    const addUser = (e) => {
-        e.preventDefault();
-        axios.post(userUrl, {
-            userId : userId.current.value,
-            userAddress : userAddress.current.value,
-            userPassword : userPassword.current.value,
-            userAdmin : userAdmin.current.value
-        })
-            
-    }
+	const addUser = (e) => {
+		e.preventDefault();
+		axios.post(userUrl, {
+			userId: userId.current.value,
+			userAddress: userAddress.current.value,
+			userPassword: userPassword.current.value,
+			userAdmin: false
+		})
 
-    return ( 
-        
+	}
 
-    <form className="row g-3">
-            <form onSubmit={addUser}>
+	return (
 
-            <div className="col-md-4">
-                <label for="inputId" className="form-label">ID</label>
-                <input type="text" className="form-control" id="inputAddress2" placeholder="사용하실 아이디를 입력해주세요." ref={userId}/>
-            </div>
 
-                <tr />
+		<form className="row g-3">
+			<form onSubmit={addUser}>
 
-            <div className="col-md-6">
-                <label for="inputPassword4" className="form-label">Password</label>
-                <input type="password" className="form-control" id="inputPassword4" placeholder="사용하실 비밀번호를 입력하세요." ref={userPassword}/>
-            </div>
+				<div className="col-md-4">
+					<label for="inputId" className="form-label">ID</label>
+					<input type="text" className="form-control" id="inputAddress2" placeholder="사용하실 아이디를 입력해주세요." ref={userId} />
+				</div>
 
-            <tr />
+				<tr />
 
-            <div className="col-md-6">
-                <label for="inputPassword4" className="form-label">Password Confirm</label>
-                <input type="password" className="form-control" id="inputPassword4" placeholder="비밀번호를 한 번 더 입력해주세요."/>
-            </div>
+				<div className="col-md-6">
+					<label for="inputPassword4" className="form-label">Password</label>
+					<input type="password" className="form-control" id="inputPassword4" placeholder="사용하실 비밀번호를 입력하세요." ref={userPassword} />
+				</div>
 
-            <div className="col-12">
-                <label for="inputAddress" className="form-label">Address</label>
-                <input type="text" className="form-control" id="inputAddress" placeholder="주소를 입력해주세요." ref={userAddress}/>
-            </div>
-            
-            <div className="col-12">
-                <button type="submit" className="btn btn-primary">Sign in</button>
-            </div>
+				<tr />
 
-            </form>
+				<div className="col-md-6">
+					<label for="inputPassword4" className="form-label">Password Confirm</label>
+					<input type="password" className="form-control" id="inputPassword4" placeholder="비밀번호를 한 번 더 입력해주세요." />
+				</div>
 
-    </form>
+				<div className="col-12">
+					<label for="inputAddress" className="form-label">Address</label>
+					<input type="text" className="form-control" id="inputAddress" placeholder="주소를 입력해주세요." ref={userAddress} />
+				</div>
 
-     );
+				<div className="col-12">
+					<button type="submit" className="btn btn-primary">Sign in</button>
+				</div>
+
+			</form>
+
+		</form>
+
+	);
 }
 
 export default Join;
--- reactapp/src/components/page/Login.js
+++ reactapp/src/components/page/Login.js
@@ -3,32 +3,32 @@ import React, { useRef } from "react";
 
 function Login() {
 
-    const userUrl = "http://localhost:8080/user/find/{id}";
-    const userId = useRef();
-    const userPassword = useRef();
+	const userUrl = "http://localhost:8080/user/find/";
+	const userId = useRef();
+	const userPassword = useRef();
 
-    const addSignup = (e) => {
-        e.preventDefault();
-        axios.post(userUrl, {
-            userId: userId.current.value,
-            userPassword: userPassword.current.value
-        })
-    }
+	const addSignup = (e) => {
+		e.preventDefault();
+		axios.post(userUrl + userId, {
+			userId: userId.current.value,
+			userPassword: userPassword.current.value
+		})
+	}
 
-    return ( 
+	return (
 
-    <form onSubmit={addSignup}>
-        <div className="col-md-4">
-        <label for="InputId" className="form-label">ID</label>
-        <input type="text" className="form-control" id="Id" placeholder="아이디를 입력하세요." ref={userId} />
-        </div>
-        <div className="col-md-4">
-        <label for="InputPassword" className="form-label">PASSWORD</label>
-        <input type="text" className="form-control" id="Password" placeholder="비밀번호를 입력하세요." ref={userPassword} />
-        </div>
-        <button class="btn btn-primary" type="button" >Login</button>
-    </form>
-     );
+		<form onSubmit={addSignup}>
+			<div className="col-md-4">
+				<label for="InputId" className="form-label">ID</label>
+				<input type="text" className="form-control" id="Id" placeholder="아이디를 입력하세요." ref={userId} />
+			</div>
+			<div className="col-md-4">
+				<label for="InputPassword" className="form-label">PASSWORD</label>
+				<input type="text" className="form-control" id="Password" placeholder="비밀번호를 입력하세요." ref={userPassword} />
+			</div>
+			<button class="btn btn-primary" type="button" >Login</button>
+		</form>
+	);
 }
 
 export default Login;

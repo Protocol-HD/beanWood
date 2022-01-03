053b4d6811dbfead9dac127ccf9b1be7cb10aed1
hyowon <projectann215@gmail.com>
2022-01-03 11:24:09 +0900

    color, image, product log update

--- springBoot/src/main/java/beanWood/springBoot/image/service/ImageServiceImpl.java
+++ springBoot/src/main/java/beanWood/springBoot/image/service/ImageServiceImpl.java
@@ -3,11 +3,13 @@ package beanWood.springBoot.image.service;
 import beanWood.springBoot.image.model.Image;
 import beanWood.springBoot.image.repository.ImageRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ImageServiceImpl implements ImageService {
@@ -15,21 +17,25 @@ public class ImageServiceImpl implements ImageService {
 
     @Override
     public Image saveImage(Image image) {
+        log.info("save Image successfully: {}", image);
         return imageRepository.save(image);
     }
 
     @Override
     public Optional<Image> findByIdImage(Long id) {
+        log.info("find by id Image: {}", id);
         return imageRepository.findById(id);
     }
 
     @Override
     public List<Image> findAllImage() {
+        log.info("find all Image");
         return imageRepository.findAll();
     }
 
     @Override
     public void deleteByIdImage(Long id) {
+        log.info("delete by id Image: {}", id);
         imageRepository.deleteById(id);
     }
 }
--- springBoot/src/main/java/beanWood/springBoot/product/service/ProductServiceImpl.java
+++ springBoot/src/main/java/beanWood/springBoot/product/service/ProductServiceImpl.java
@@ -5,11 +5,13 @@ import beanWood.springBoot.product.dto.IProduct;
 import beanWood.springBoot.product.model.Product;
 import beanWood.springBoot.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
@@ -18,6 +20,15 @@ public class ProductServiceImpl implements ProductService {
 
 	@Override
 	public Product saveProduct(IProduct iProduct) {
+		log.info("Save Product Successfully:" +
+				"id : {}", iProduct.getId() +
+				"description: {}",iProduct.getDescription() +
+				"sale: {}", iProduct.getSale() +
+				"product name: {}", iProduct.getProductName() +
+				"star: {}", iProduct.getStar() +
+				"price: {}", iProduct.getPrice() +
+				"category: {}", iProduct.getCategoryId()
+		);
 		return productRepository.save(
 				Product.builder()
 						.id(iProduct.getId())
@@ -34,21 +45,25 @@ public class ProductServiceImpl implements ProductService {
 
 	@Override
 	public Optional<Product> findByIdProduct(Long id) {
+		log.info("find by id Product: {}", id);
 		return productRepository.findById(id);
 	}
 
 	@Override
 	public List<Product> findAllProduct() {
+		log.info("find all Product");
 		return productRepository.findAll();
 	}
 
 	@Override
 	public void deleteByIdProduct(Long id) {
+		log.info("delete by id Product: {}", id);
 		productRepository.deleteById(id);
 	}
 
 	@Override
 	public List<Product> findByCategoryId(Long categoryId) {
+		log.info("find by category id Product: {}", categoryId);
 		return productRepository.findByCategoryId(categoryId);
 	}
 }

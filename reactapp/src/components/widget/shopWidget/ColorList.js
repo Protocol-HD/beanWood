65849c4df9a4042bf61820cee280ac6760100643
hjm8333 <hjm8333@naver.com>
2022-01-04 15:56:03 +0900

    asd

--- reactapp/src/components/widget/shopWidget/ColorList.js
+++ reactapp/src/components/widget/shopWidget/ColorList.js
@@ -1,13 +1,74 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useRef } from 'react';
 
-function ColorList({item, check}) {
-    console.log(item.id)
+function ColorList({ item, check, setCheck }) {
+    const editColorName = useRef();
+
+    const deleteColor = () => {
+        const deleteUrl = `http://localhost:8080/color/delete/${item.id}`;
+        if (
+            window.confirm('color : "' + item.colorName + '" 삭제하시겠습니까?')
+        ) {
+            axios.delete(deleteUrl).then((res) => {
+                setCheck(!check);
+            });
+        } else {
+            window.alert('삭제되지 않았습니다');
+        }
+    };
+
+    const editColor = () => {
+        if (editColorName.current.value !== '') {
+            const editUrl = 'http://localhost:8080/color/update';
+            if (
+                window.confirm(
+                    'color : "' +
+                        item.colorName +
+                        '"을 "' +
+                        editColorName.current.value +
+                        '"로 수정하시겠습니까?'
+                )
+            ) {
+                axios
+                    .put(editUrl, {
+                        ...item,
+                        colorName: editColorName.current.value,
+                    })
+                    .then((res) => {
+                        setCheck(!check);
+                    });
+            } else {
+                window.alert('수정되지 않았습니다');
+            }
+        }
+    };
+
     return (
-        <tr key = {item.id}>
-        <td className='color_no'>{item.id}</td>
-        <td className='color_name'>{item.colorName}</td>
+        <tr key={item.id}>
+            <td className='color_no'>{item.id}</td>
+            <td className='color_name'>{item.colorName}</td>
+            <td className='color_delete'>
+                <button onClick={deleteColor} className='btn btn-secondary'>
+                    DEL
+                </button>
+            </td>
+            <td className='color_edit'>
+                <input
+                    type='text'
+                    className='form-control'
+                    id='colorName'
+                    placeholder='컬러 이름 입력'
+                    required=''
+                    ref={editColorName}
+                />
+            </td>
+            <td className='color_edit'>
+                <button onClick={editColor} className='btn btn-secondary'>
+                    Edit
+                </button>
+            </td>
         </tr>
-     );
+    );
 }
 
 export default ColorList;

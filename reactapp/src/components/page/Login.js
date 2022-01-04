fa83f39b588f211b0839faa33982dad93d56c992
Gwak-daeun <93602351+Gwak-daeun@users.noreply.github.com>
2022-01-04 11:53:43 +0900

    signUp, login

--- reactapp/src/components/page/Login.js
+++ reactapp/src/components/page/Login.js
@@ -1,12 +1,17 @@
 import axios from "axios";
 import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDoorOpen } from "@fortawesome/free-solid-svg-icons";
 
 function Login() {
-	const userUrl = "http://localhost:8080/user/findByUserId/";
-	const userId = useRef();
-	const userPassword = useRef();
 
-	const addSignup = (e) => {
+    const userUrl = "http://localhost:8080/user/findByUserId/";
+    const userId = useRef();
+    const userPassword = useRef();
+    
+
+    const addSignup = (e) => {
 		e.preventDefault();
 		axios.get(userUrl + userId.current.value).then(Response => {
 			console.log(Response.data);
@@ -17,23 +22,41 @@ function Login() {
 			}
 		})
 	}
-	return (
-		<form onSubmit={addSignup}>
-			<div className="col-6 m-auto" >
-				<label htmlFor="InputId" className="form-label">ID</label>
-				<input type="text" className="form-control" id="Id" placeholder="아이디를 입력하세요." ref={userId} />
-			</div>
 
-			<div className="col-6 m-auto" >
-				<label htmlFor="InputPassword" className="form-label">PASSWORD</label>
-				<input type="password" className="form-control" id="Password" placeholder="비밀번호를 입력하세요." ref={userPassword} />
-			</div>
-
-			<div className="text-center mt-3">
-				<button className="btn btn-primary" type="submit" >Login</button>
-			</div>
-		</form>
-	);
+    return ( 
+        <section className="login-area pt-100 pb-100">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-8 offset-lg-2">
+                        <div className="basic-login">
+                        <div className="text-center">
+                        <FontAwesomeIcon icon={faDoorOpen} size="3x" color="#003333" />
+                        </div>
+                            <h3 className="text-center mb-60">Login From Here</h3>
+                            <form onSubmit={addSignup}>
+                                <label for="name">ID<span>**</span></label>
+                                <input id="name" type="text" placeholder="아이디를 입력하세요." ref={userId}/>
+                                <label for="pass">PASSWORD <span>**</span></label>
+                                <input id="pass" type="password" placeholder="비밀번호를 입력하세요." ref={userPassword}/>
+                                <div className="login-action mb-20 fix">
+                                    <span className="log-rem f-left">
+                                        <input id="remember" type="checkbox" />
+                                        <label for="remember">Remember me!</label>
+                                    </span>
+                                    <span className="forgot-login f-right">
+                                        <a href="javascript:void(0)">Lost your password?</a>
+                                    </span>
+                                </div>
+                                <button className="btn theme-btn-2 w-100">로그인 하기</button>
+                                <div className="or-divide"><span>or</span></div>
+                                <button className="btn theme-btn w-100"><Link to="/signup">회원가입 하기</Link></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+     );
 }
 
 export default Login;
---
+++ reactapp/src/components/page/SignUp.js
@@ -0,0 +1,101 @@
+import axios from "axios";
+import React, { useCallback, useRef } from "react";
+import { Link } from "react-router-dom";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faUserPlus} from '@fortawesome/free-solid-svg-icons';
+import { useState } from "react/cjs/react.development";
+import {Button} from 'react-bootstrap';
+
+
+
+function SignUp() {
+    const userUrl = "http://localhost:8080/user/save";
+    const userIdCheckUrl = "http://localhost:8080/user/checkUserId/{id}/exists";
+
+    const userId = useRef();
+	const userAddress = useRef();
+	const userPassword = useRef();
+
+    const [idCheck, setIdCheck] = useState('');
+    const [idCheckError, setIdCheckError] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordCheck, setPasswordCheck] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+
+
+    // 회원가입
+    const addUser = (e) => {
+		e.preventDefault();
+        if(password !== passwordCheck){
+            return setPasswordError(true);
+        }
+		axios.post(userUrl, {
+			userId: userId.current.value,
+			userAddress: userAddress.current.value,
+			userPassword: userPassword.current.value,
+			userAdmin: false
+		});
+        
+	}
+    //회원가입end
+
+    //아이디 중복 체크
+    // const checkUserId = (e) => {
+    //     e.preventDefault();
+       
+    // }
+    //아이디 중복 체크 end
+
+    const onChangePassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const onChangePasswordCheck = (e) => {
+        setPasswordError(e.target.value !== password);
+        setPasswordCheck(e.target.value);
+    }
+
+    return ( 
+        <section className="login-area pt-100 pb-100">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-8 offset-lg-2">
+                        <div className="basic-login">
+                        <div className="text-center">
+                        <FontAwesomeIcon icon={faUserPlus} className="user" size="3x" color="#003333" />
+                        </div>
+                            <h3 className="text-center mb-60">Signup From Here</h3>
+                            <form onSubmit={addUser}>
+                                <form onSubmit={checkUserId}>
+                                <label for="id">ID <span>**</span></label>
+                                <input id="id" type="text" placeholder="사용하실 아이디를 입력해주세요." ref={userId} />
+                                <button className="btn theme-btn" onClick={checkUserId}>ID 중복 확인</button>
+                                </form>
+                                <label for="password">Password <span>**</span></label>
+                                <input id="password" type="password" placeholder="사용하실 비밀번호를 입력하세요." ref={userPassword} onChange={onChangePassword}/>
+
+                                <label for="passwordconfirm">Password Confirm <span>**</span></label>
+                                <div>
+                                <input id="user-password-check" type="password" value={passwordCheck} onChange={onChangePasswordCheck} placeholder="사용하실 비밀번호를 한 번 더 입력하세요."  />
+                                {passwordError && <div style={{color : 'red'}}>
+                                    비밀번호가 일치하지 않습니다.</div>}
+                                </div>
+                                
+                                <label for="pass">Address <span>**</span></label>
+                                <input id="pass" type="text" placeholder="사용하실 주소를 입력하세요." ref={userAddress} />
+
+                                <div className="mt-10"></div>
+                                <button className="btn theme-btn-2 w-100" >회원가입하기</button>
+                                <div className="or-divide"><span>or</span></div>
+                                <button className="btn theme-btn w-100"><Link to="/login">로그인 하기</Link></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+     );
+}
+
+
+export default SignUp;
